4cfa0ed12446b4365bb4d8757145c2bc16bd6d34
gilsonsilven <gilsonsilven@gmail.com>
2025-05-10 19:28:16 -0300

    feat: sign In page

---
+++ src/app/signIn/page.jsx
@@ -0,0 +1,34 @@
+import Home from "../page"
+
+import HomepageImage from "@/components/HomepageImage.jsx"
+
+
+export default function SignIn() {
+    return (
+        <div className="flex bg-white h-screen w-screen">
+            <div className="w-1/2">
+                <div className="flex flex-col justify-center items-center h-full">
+                    <div>
+                        <h1 className="text-3xl font-semibold text-gray-800 mb-8">Acesse sua conta</h1>
+                    </div>
+                    <div>
+                        <input id="email" type="email" placeholder="Email" className="border w-[300px] placeholder:text-black placeholder:opacity-60 border-[#d1d5db] rounded pl-3 py-2 border-opacity-30 hover:border-gray-700 my-2.5"></input>
+                    </div>
+                    <div>
+                        <input id="pass" type="password" placeholder="Senha" className="border w-[300px] placeholder:text-black placeholder:opacity-60 border-[#d1d5db] rounded pl-3 py-2 border-opacity-30 hover:border-gray-700 my-2.5"></input>
+                    </div>
+                    <div>
+                        <button className="font-semibold mt-4 mb-2 w-[300px] h-[36px] rounded bg-blue-500 text-white hover:cursor-pointer">Entrar</button>
+                    </div>
+                    <div className="mb-10">
+                        <p>Não possui uma conta? Cadastre-se</p>
+                    </div>
+                </div>
+            </div>
+            <div className="w-1/2">
+                <HomepageImage />
+            </div>
+        </div>
+    );
+    
+};
