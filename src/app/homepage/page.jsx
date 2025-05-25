0e9915fee0ec0f6b7a54fe536444c1272112a214
gilsonsilven <gilsonsilven@gmail.com>
2025-05-25 15:23:26 -0300

    feat: scheduler

--- src/app/homepage/page.jsx
+++ src/app/homepage/page.jsx
@@ -1,11 +1,14 @@
+
+
 import PageLayout from "@/components/PageLayout.jsx"
+import HomePageScheduler from "@/components/HomePageScheduler.jsx"
 
 export default function Homepage() {
 
     return (
 
         <PageLayout>
-           
+           <HomePageScheduler />
         </PageLayout>
 
     )
---
+++ src/components/HomePageScheduler.jsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { Scheduler } from "@aldabil/react-scheduler";
+
+export default function HomePageScheduler() {
+    return (
+        <Scheduler
+  view="month"
+  events={[
+    {
+      event_id: 1,
+      title: "Event 1",
+      start: new Date("2021/5/2 09:30"),
+      end: new Date("2021/5/2 10:30"),
+    },
+    {
+      event_id: 2,
+      title: "Event 2",
+      start: new Date("2021/5/4 10:00"),
+      end: new Date("2021/5/4 11:00"),
+    },
+  ]}
+/>
+    )
+
+}
