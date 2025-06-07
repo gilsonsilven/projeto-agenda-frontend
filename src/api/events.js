c1a7ceeb7fb6df415afc0424b7067fb22b0632bd
gilsonsilven <gilsonsilven@gmail.com>
2025-06-07 12:38:46 -0300

    feat: event api function && scheduler crud implementation

---
+++ src/api/events.js
@@ -0,0 +1,129 @@
+
+
+
+const API_BASE_URL = 'http://localhost:3001';
+/// mudar a lógica para pegar o id do usuário que está logado
+// id do usuário não está em eventData
+const id_user = 1; // Placeholder for user ID, replace with actual logic to get logged-in user ID
+
+export async function createEvent(eventData) {
+
+    const data = {
+        ...eventData,
+        event_start_date: eventData.start,
+        event_end_date: eventData.end,
+        contact_names: eventData.contacts.join(',') // converte de array de strings para uma única string
+    };
+
+    delete data.contacts; 
+    delete data.start
+    delete data.end
+
+    console.log("api event linha 17:", data);
+ 
+
+    const response = await fetch(`${API_BASE_URL}/events/user/${id_user}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+
+    const result = await response.json();
+
+    
+    return result;
+
+}
+
+
+export async function getEvents() {
+
+
+    const response = await fetch(`${API_BASE_URL}/events/user/${id_user}/list`, {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        cache: 'no-store'
+
+    });
+
+    const result = await response.json();
+
+    /// tratar caso não tenha eventos
+    /// tratar propriedades pois se não tiver o nome correto, o scheduler não renderiza
+    const formattedEvents = result.map(
+        ({ event_start_date, event_end_date, contact_names, id_event, ...event }) => ({
+        ...event,
+        start: new Date(event_start_date),
+        end: new Date(event_end_date),
+        contacts: contact_names ? contact_names.split(',') : [], // passa de string para array de strings
+        event_id: id_event
+        //contacts: event.contact_names ? event.contact_names.split(',') : []
+
+    }));
+
+    
+    return formattedEvents;
+
+}
+
+
+export async function updateEvent(eventData) {
+
+    const data = {
+        ...eventData,
+        event_start_date: eventData.start,
+        event_end_date: eventData.end,
+        contact_names: eventData.contacts.join(','),
+        id_event: eventData.event_id
+    };
+
+    delete data.contacts; 
+    delete data.start;
+    delete data.end;
+    delete data.event_id;
+
+    console.log("api event linha 17:", data.contact_names);
+ 
+
+    const response = await fetch(`${API_BASE_URL}/events/user/${id_user}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+
+    const result = await response.json();
+
+    
+    return result;
+
+}
+
+
+export async function deleteEvent(event_id) {
+
+    const id_event = event_id;
+
+    console.log("api event delete linha 122:", id_event);
+ 
+
+    const response = await fetch(`${API_BASE_URL}/events/user/${id_user}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({id_event})
+    });
+
+    const result = await response.json();
+
+    
+    return result;
+
+}
+
+
--- src/components/HomePageScheduler.jsx
+++ src/components/HomePageScheduler.jsx
@@ -1,49 +1,135 @@
 'use client';
 
 import { Scheduler } from "@aldabil/react-scheduler";
-
+import { createEvent, getEvents, updateEvent, deleteEvent } from "@/api/events.js";
+import { getContacts } from "@/api/contact.js";
+import { useEffect, useState } from "react";
 
 export default function HomePageScheduler() {
-    return (
-        <Scheduler
-  view="month"
-  agenda={false}
 
-  ////teste
+  const [eventList, setEventList] = useState([]);
+  const [deletedEventFlag, setDeletedEventFlag] = useState(false);
+  const [contactList, setContactList] = useState([]);
 
-  fields={[
-    {
-      name: "user_id",
-      type: "select",
-      // Should provide options with type:"select"
-      options: [
-        { id: 1, text: "John", value: 1 },
-        { id: 2, text: "Mark", value: 2 }
-      ],
-      config: { label: "User", required: true, errMsg: "Plz Select User" }
-    },
-    {
-      name: "Description",
-      type: "input",
-      default: "Default Value...",
-      config: { label: "Details", multiline: true, rows: 4 }
-    },
-    {
-      name: "anotherdate",
-      type: "date",
-      config: {
-        label: "Other Date",
-        md: 6,
-        type: "datetime"
-      }
+
+  useEffect(() => {
+    const loadEventList = async () => {
+      const data = await getEvents();
+      /// tratar aqui depois
+      console.log(data);
+
+      
+      setEventList(data)
+    };
+
+    loadEventList();
+      
+  }, [deletedEventFlag]);
+  
+
+  useEffect(() => {
+    const loadContacts = async () => {
+      const result = await getContacts(); 
+      const data = result.map((c) => ({
+        id: c.id_contact,
+        text: c.name,
+        value: c.name
+      }));
+      setContactList(data);
+    };
+
+    loadContacts();
+    
+  }, []);  
+
+
+  const handleSubmit = async (event) => {
+
+    if(event.event_id === null) {
+      const {subtitle, Start, event_id, ...data} = event
+
+      const response = await createEvent(data);
+
+      console.log("Event created: linha 15", response);
+
+      return {event_id: response.event_id, ...event};
     }
-  ]}
+    else {
 
-  //////
+      const {subtitle, Start, ...data} = event
+
+      const response = await updateEvent(data);
+
+      console.log("Event updated: linha 15", response);
+
+      return {event_id: response.event_id, ...event};      
+
+    }
+    //return {event_id: 1, ...event}
+  } 
+
+  const handleDelete = async (event) => {
+
+    console.log("handleDelete: linha 54", event);
+
+    //const {subtitle, Start, ...data} = event
+
+    const response = await deleteEvent(event);
+
+    console.log("Event deleted: linha 15", response);
+
+    setDeletedEventFlag(true);
+
+    return {event_id: response.event_id, ...event};     
+  }
 
 
- 
+  return (
+
+    <Scheduler
+      view="month"
+      agenda={false}
+      day={false}
+      //week={false}
+      onConfirm={handleSubmit}
+      onDelete={handleDelete}
+      events={eventList}
+      editable={true}
+      fields={[
+        {
+          name: "subtitle",
+          disabled: true,
+        },
+        {
+          name: "address",
+          type: "input",
+          config: {
+            label: "Endereço"
+          }
+        },
+        {
+          name: "Start"
+        },          
+        {
+          name: "description",
+          type: "input",
+          config: { label: "Descrição", multiline: true, rows: 4 }
+        },
+        {
+          name: "contacts",
+          type: "select",
+         
+          
+          // Should provide options with type:"select"
+          // carregar a lista de contatos do usuário logado
+          // depois
+          options: contactList,
+          config: { label: "Contatos", multiple: true}
+        },          
+      ]}
+      key={contactList.length}
+    
     />
-    );
+  );
 
 }
